7773df8259f760e09b57c1677ef0ed796ee7a697
khdoop <botscast@mail.bg>
2016-08-27 22:15:46 +0300

    init

---
+++ src/app/shared/card.ts
@@ -0,0 +1,10 @@
+export interface Card {
+	/*attack?: number;
+	 defense?: number;
+	 id?: number;
+	 name?: string;
+	 score?: number;
+	 type?: string;*/
+	x: number;
+	y: number;
+}
---
+++ src/vendor.ts
@@ -0,0 +1,21 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import 'rxjs';
+import '@angularclass/hmr';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import 'node_modules/jquery/dist/jquery.min.js';
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'node_modules/bootstrap/dist/js/bootstrap.min.js';
+import 'node_modules/bootstrap/dist/css/bootstrap2.min.css';
+
+import 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js';
+import 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
+import 'node_modules/malihu-custom-scrollbar-plugin/mCSB_buttons.png';
